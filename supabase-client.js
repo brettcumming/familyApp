08831f57d4745e35976ffd1798847686bcf08464
bc <cumming.brett@gmail.com>
2026-08-02 09:22:06 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,5 @@
 // supabase-client.js — include this before auth.js on every page
-const SUPABASE_URL = 'https://YOUR_PROJECT_REF.supabase.co';
+const SUPABASE_URL = 'https://bozmirtijielsilbljsv.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'sb_publishable_SJjOnnDTroqbsFy1HNpMkQ_26Y2lx8a';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
